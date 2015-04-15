c40429a87f2edb4273f58cdb63905ce978fc6cdf
Ramya Thulasingam <pair+ramya@book.com>
2015-04-15 15:32:59 -0700

    changed the time zone to pst

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   end
 
   def large_image
-    @photos = FLICKR.photos.search(:user_id => '130728377@N08',  :min_upload_date => (Time.now - 15*60).to_s(:db) ).collect{|i|i.url(size = "large" )}
+    @photos = FLICKR.photos.search(:user_id => '130728377@N08',  :min_upload_date => (Time.now - 15*60).in_time_zone("Pacific Time (US & Canada)").strftime("%Y-%m-%d %H:%M:%S") ).collect{|i|i.url(size = "large" )}
     render json: @photos
   end
 end
